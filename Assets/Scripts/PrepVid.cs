c8d2bc7f28793924b17aec0e822cfa15bfacb01b
ipowerilya <idestinyilya@gmail.com>
2020-07-20 15:14:38 +0300

    commit

---
+++ Assets/Scripts/PrepVid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PrepVid : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!videoPlayer.isPrepared)
+        {
+            videoPlayer.Prepare();
+        }
+    }
+}
